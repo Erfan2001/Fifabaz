74e8fd98a286ccfcbcb85fb23fbfe7c31300ba5b
Erfan <1380erfan2001@gmail.com>
2020-10-21 23:43:56 +0330

    fifabaz

--- src/components/SortBox.js
+++ src/components/SortBox.js
@@ -4,33 +4,31 @@ class SortBox extends React.Component {
   state={
     age:"btn-outline-primary",
     short_name:"btn-outline-primary",
-    value:"btn-outline-primary",
-    last:""
+    value:"btn-outline-primary"
   }
   fields = ["age", "short_name", "value"];
   classes = ["btn-outline-primary", "btn-info", "btn-success"];
   setClassName = (e) => {
     const {id,className}=e.target;
     const [btnOutlinePrimary,btnInfo,btnSuccess]=this.classes;
-    this.setState({last:id});
     let a=this.fields.filter(field=>field!=id);
     switch(className.split(" ")[2]){
       //1
       case btnInfo:
         this.setState({[id]:btnOutlinePrimary,[a[0]]:btnOutlinePrimary,[a[1]]:btnOutlinePrimary},()=>{
-          this.props.changeSort(this.state[this.state.last],this.state.last);
+          this.props.changeSort(this.state[id],id);
         })
         break;
       //2
       case btnSuccess:
         this.setState({[id]:btnInfo,[a[0]]:btnOutlinePrimary,[a[1]]:btnOutlinePrimary},()=>{
-          this.props.changeSort(this.state[this.state.last],this.state.last);
+          this.props.changeSort(this.state[id],id);
         })
         break;
       //3
         case btnOutlinePrimary:
           this.setState({[id]:btnSuccess,[a[0]]:btnOutlinePrimary,[a[1]]:btnOutlinePrimary},()=>{
-            this.props.changeSort(this.state[this.state.last],this.state.last);
+            this.props.changeSort(this.state[id],id);
           })
           break;
     }
